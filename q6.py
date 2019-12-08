8db57883b6e74c3483bf5d1a07c96860a1c2abb3
OKAKI_SE <reeeiapp@gmail.com>
2019-12-08 23:58:46 +0900

    add file

---
+++ q6.py
@@ -0,0 +1,13 @@
+#FizzBuzz問題
+#問題：1〜100までの数字のうち、3で割り切れるものは"Fizz!",5で割り切れるものは"Buzz!",
+#15で割り切れるものは"FizzBuzz!"と表示させ、それ以外の数はそのままの数を表示させなさい。　　
+
+for i in range(1,101):
+    if i%15 == 0:
+        print("FizzBuzz!")
+    elif i%3 ==0:
+        print("Fizz!")
+    elif i%5 ==0:
+        print("Buzz!")
+    else:
+        print(i)
